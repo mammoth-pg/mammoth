b1184ade5c418c440372550651a3340a780665a6
Arthur Pirogovski <arthur@flyingtealeaf.com>
2014-10-18 17:22:39 +0300

    adding models to Ruby search path (dunno what is Lotus-specific way to do this)

--- application.rb
+++ application.rb
@@ -17,3 +17,6 @@ module Mammoth
     end
   end
 end
+
+# FIXME(arp): hopefully Lotus has a better way of loading files...
+$LOAD_PATH.unshift(::Mammoth::Application.configuration.root.join('app', 'models'))
