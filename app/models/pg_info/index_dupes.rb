71ef42e12f91e8c8d6448cb29453a649f229ba45
Arthur Pirogovski <arthur@flyingtealeaf.com>
2014-10-19 02:58:40 +0300

    index dupes added

---
+++ app/models/pg_info/index_dupes.rb
@@ -0,0 +1,19 @@
+require 'pg_info/stats'
+
+module PgInfo
+  class IndexDupes < ::PgInfo::Stats
+    def get(options = {})
+      return sql <<-END
+        SELECT pg_size_pretty(sum(pg_relation_size(idx))::bigint) AS size,
+               (array_agg(idx))[1] AS idx1, (array_agg(idx))[2] AS idx2,
+               (array_agg(idx))[3] AS idx3, (array_agg(idx))[4] AS idx4
+        FROM (
+            SELECT indexrelid::regclass AS idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
+                                                 coalesce(indexprs::text,'')||E'\n' || coalesce(indpred::text,'')) AS KEY
+            FROM pg_index) sub
+        GROUP BY KEY HAVING count(*)>1
+        ORDER BY sum(pg_relation_size(idx)) DESC
+      END
+    end
+  end
+end
