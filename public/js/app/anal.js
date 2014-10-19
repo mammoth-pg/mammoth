9d6afa5e72fa1fd0d31c565fdc96ee3208f77f0c
Andrey Savchenko <andrey@aejis.eu>
2014-10-19 20:33:06 +0300

    Table rendering

---
+++ public/js/app/anal.js
@@ -0,0 +1,26 @@
+require(['ractive', 'lib/net'], function(Ractive, net) {
+  'use strict';
+
+  var stats = [],
+      statType = window.document.getElementById('table').dataset.type,
+      table;
+
+  table = new Ractive({
+    el: 'data',
+    template: '#template'
+  });
+
+  function getStats() {
+    var request = new net.Request('get', '/pg_info/' + statType + '.json').send();
+
+    request.then(function(resp) {
+      table.set({data: resp.json});
+
+      setTimeout(function() {
+        getStats();
+      }, 5000);
+    });
+  }
+
+  getStats();
+});
