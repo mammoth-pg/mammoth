3563525b338000474e5f55a9e1a6e5435f216a05
Arthur Pirogovski <arthur@flyingtealeaf.com>
2014-10-20 02:50:05 +0300

    assets

--- public/js/app/main.js
+++
@@ -1,4 +0,0 @@
-require(function() {
-  'use strict';
-
-});
--- public/js/require_config.js
+++
@@ -1,7 +0,0 @@
-requirejs.config({
-  baseUrl: '/js',
-  paths: {
-    'ractive': 'vendor/ractive',
-    'ace':     'vendor/ace/lib/ace'
-  }
-});
